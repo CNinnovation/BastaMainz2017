a2575bfc24ee9fa33d705d584578ff578999c2fc
Christian Nagel <christian@christiannagel.com>
2017-09-25 21:59:02 +0200

    dependency injection samples

---
+++ DI/DependencyInjectionSamples/DIWithOptions/GreetingServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DIWithOptions
+{
+    public static class GreetingServiceExtensions
+    {
+        public static IServiceCollection AddGreetingService(this IServiceCollection services,
+            Action<GreetingServiceOptions> setupAction)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (setupAction == null) throw new ArgumentNullException(nameof(setupAction));
+
+            services.Configure(setupAction);
+            return services.AddTransient<IGreetingService, GreetingService>();
+        }
+    }
+}
