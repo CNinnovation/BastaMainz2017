c59d3851ffaee678aa99c597629dc47b68d9c4ef
Christian Nagel <christian@christiannagel.com>
2017-09-25 21:31:09 +0200

    DI - no DI

---
+++ DI/Intro/NoDI/NoDI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NoDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new HomeController();
+            string message = controller.Hello("Stephanie");
+            Console.WriteLine(message);
+        }
+    }
+}
