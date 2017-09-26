d98201ae0a1634054818813715be1b6dfc63510c
Christian Nagel <christian@christiannagel.com>
2017-09-26 13:03:15 +0200

    ASP.NET Core with DI

---
+++ DI/WebApp/WebApp/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IGreetingService _greetingService;
+        public ValuesController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        [HttpGet]
+        public string Get(string name) =>
+            _greetingService.Greet(name);
+    }
+}
